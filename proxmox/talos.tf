d18525c6a72b695d5ae891c17eb23a6d39941b09
Christian Kleinbölting <christian@kleinb.org>
2026-04-15 00:23:20 +0200

    feat: add Tailscale egress and CoreDNS rewrites for Authentik
    Add Tailscale egress services for Authentik with ClusterIP routing through tailscale namespace. Configure CoreDNS to rewrite authentik.lungfish-ide.ts.net to the internal egress ClusterIP service, enabling external Tailscale access. Update Mealie OIDC configuration URL to use Tailscale FQDN instead of internal cluster DNS.

--- proxmox/talos.tf
+++ proxmox/talos.tf
@@ -2,6 +2,42 @@ locals {
   cp_ip            = [for addr in flatten(proxmox_virtual_environment_vm.talos_control_plane.ipv4_addresses) : addr if addr != "127.0.0.1" && !startswith(addr, "169.254.") && !startswith(addr, "10.244.")][0]
   worker_ips       = [for vm in proxmox_virtual_environment_vm.talos_worker : [for addr in flatten(vm.ipv4_addresses) : addr if addr != "127.0.0.1" && !startswith(addr, "169.254.") && !startswith(addr, "10.244.")][0]]
   cluster_endpoint = "https://${local.cp_ip}:6443"
+
+  coredns_corefile = <<-EOT
+.:53 {
+    errors
+    health {
+        lameduck 5s
+    }
+    ready
+    log . {
+        class error
+    }
+    prometheus :9153
+
+    rewrite name exact authentik.lungfish-ide.ts.net authentik-egress-clusterip.tailscale.svc.cluster.local
+
+    hosts {
+        192.168.178.87 git.organa.one
+        fallthrough
+    }
+    kubernetes cluster.local in-addr.arpa ip6.arpa {
+        pods insecure
+        fallthrough in-addr.arpa ip6.arpa
+        ttl 30
+    }
+    forward . /etc/resolv.conf {
+       max_concurrent 1000
+    }
+    cache 30 {
+       disable success cluster.local
+       disable denial cluster.local
+    }
+    loop
+    reload
+    loadbalance
+}
+EOT
 }
 
 resource "talos_machine_secrets" "this" {}
@@ -31,6 +67,28 @@ resource "talos_machine_configuration_apply" "controlplane" {
   client_configuration        = talos_machine_secrets.this.client_configuration
   machine_configuration_input = data.talos_machine_configuration.controlplane.machine_configuration
   node                        = local.cp_ip
+  config_patches = [
+    yamlencode({
+      cluster = {
+        inlineManifests = [
+          {
+            name     = "coredns-config"
+            contents = yamlencode({
+              apiVersion = "v1"
+              kind       = "ConfigMap"
+              metadata = {
+                name      = "coredns"
+                namespace = "kube-system"
+              }
+              data = {
+                Corefile = local.coredns_corefile
+              }
+            })
+          }
+        ]
+      }
+    })
+  ]
 }
 
 resource "talos_machine_configuration_apply" "worker" {
